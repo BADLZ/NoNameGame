e2bd6bce0170f2cfa9b60521b0a4d0a018ec3657
BADLZ <48454150+BADLZ@users.noreply.github.com>
2019-03-16 18:22:40 +0000

    select character

--- Game-V2/src/Graphics/CharacterSelection.java
+++ Game-V2/src/Graphics/CharacterSelection.java
@@ -19,37 +19,41 @@ public class CharacterSelection {
 
 	private JFrame frame;
 	private int persIndex;
-	private String[] personagens = {"Cacador", "Feiticeiro", "Gladiador"};
+	private String[] personagens = { "Cacador", "Feiticeiro", "Gladiador" };
 	private JLabel nomePersonagem;
-	private ImageIcon leftarrowimg, rightarrowimg, leftarrowpressedimg, rightarrowpressedimg;
-	
+	private ImageIcon leftarrowimg, rightarrowimg, leftarrowpressedimg, rightarrowpressedimg, selectimg,
+			selectpressedimg;
+
 	private Dimension size = java.awt.Toolkit.getDefaultToolkit().getScreenSize();
 	private final int screenWidth = size.width;
 	private final int screenHeight = size.height;
-	
+
 	public CharacterSelection(JFrame frame) {
 		this.frame = frame;
 		initialize();
 		frame.setVisible(true);
 	}
 
-	private void initialize() {	
+	private void initialize() {
 		try {
 			leftarrowimg = new ImageIcon(ImageIO.read(new File("src/resources/characterSelection/leftarrow.png")));
-			rightarrowimg = new ImageIcon(
-					ImageIO.read(new File("src/resources/characterSelection/rightarrow.png")));
-			leftarrowpressedimg = new ImageIcon(ImageIO.read(new File("src/resources/characterSelection/leftarrowpressed.png")));
+			rightarrowimg = new ImageIcon(ImageIO.read(new File("src/resources/characterSelection/rightarrow.png")));
+			leftarrowpressedimg = new ImageIcon(
+					ImageIO.read(new File("src/resources/characterSelection/leftarrowpressed.png")));
 			rightarrowpressedimg = new ImageIcon(
 					ImageIO.read(new File("src/resources/characterSelection/rightarrowpressed.png")));
-		}catch (Exception e) {
+			selectimg = new ImageIcon(ImageIO.read(new File("src/resources/characterSelection/select.png")));
+			selectpressedimg = new ImageIcon(
+					ImageIO.read(new File("src/resources/characterSelection/selectpressed.png")));
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		FancyButton btnRight = new FancyButton("btnRight", screenWidth/2, screenHeight/2
-				, 97, 25,rightarrowimg,rightarrowpressedimg);
+		FancyButton btnRight = new FancyButton("btnRight", screenWidth / 2 + 133, screenHeight / 2 - 50, 35, 28,
+				rightarrowimg, rightarrowpressedimg);
 		btnRight.addKeyListener(new KeyAdapter() {
 			@Override
 			public void keyPressed(KeyEvent e) {
-				if(e.getKeyCode() == KeyEvent.VK_RIGHT)
+				if (e.getKeyCode() == KeyEvent.VK_RIGHT)
 					changeLabel(1);
 			}
 		});
@@ -60,9 +64,9 @@ public class CharacterSelection {
 			}
 		});
 		frame.getContentPane().add(btnRight);
-		
-		FancyButton btnLeft = new FancyButton("btnLeft",screenWidth/2+50, screenHeight/2
-				, 97, 25,leftarrowimg,leftarrowpressedimg);
+
+		FancyButton btnLeft = new FancyButton("btnLeft", screenWidth / 2 - 167, screenHeight / 2 - 50, 35, 28,
+				leftarrowimg, leftarrowpressedimg);
 		btnLeft.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent arg0) {
@@ -72,29 +76,29 @@ public class CharacterSelection {
 		btnLeft.addKeyListener(new KeyAdapter() {
 			@Override
 			public void keyPressed(KeyEvent e) {
-				if(e.getKeyCode() == KeyEvent.VK_LEFT)
+				if (e.getKeyCode() == KeyEvent.VK_LEFT)
 					changeLabel(-1);
 			}
 		});
 		frame.getContentPane().add(btnLeft);
-		
-		JButton btnSelect = new JButton("select");
-		btnSelect.setBounds(309, 273, 97, 25);
+
+		FancyButton btnSelect = new FancyButton("select", screenWidth / 2 - 110, screenHeight / 2 + 130, 220, 67,
+				selectimg, selectpressedimg);
 		frame.getContentPane().add(btnSelect);
-		
+
 		nomePersonagem = new JLabel(personagens[persIndex]);
 		nomePersonagem.setHorizontalAlignment(SwingConstants.CENTER);
 		nomePersonagem.setFont(new Font("Tahoma", Font.BOLD, 16));
 		nomePersonagem.setBounds(278, 169, 156, 30);
 		frame.getContentPane().add(nomePersonagem);
 	}
-	
+
 	private void changeLabel(int num) {
-		if(persIndex + num < 0) {
+		if (persIndex + num < 0) {
 			persIndex += num;
-			while(persIndex < 0)
+			while (persIndex < 0)
 				persIndex += 3;
-		}else {
+		} else {
 			persIndex = (persIndex + num) % personagens.length;
 		}
 		nomePersonagem.setText(personagens[persIndex]);
