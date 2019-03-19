485163de74b1da231f7d8d4f229ebb1accfbb62d
BADLZ <48454150+BADLZ@users.noreply.github.com>
2019-03-19 11:12:43 +0000

    e

--- Game-V2/src/testesDani/TCPClient.java
+++ Game-V2/src/testesDani/TCPClient.java
@@ -10,7 +10,7 @@ public static void main(String argv[]) throws Exception
     String sentence;
     String modifiedSentence;
     BufferedReader inFromUser =new BufferedReader(new   InputStreamReader(System.in));
-    Socket clientSocket = new Socket(InetAddress.getByName("SERVER_IP"), 6789);
+    Socket clientSocket = new Socket(InetAddress.getByName("0.0.0.0"), 6789);
     DataOutputStream outToServer =new DataOutputStream(clientSocket.getOutputStream());
     BufferedReader inFromServer =new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
     System.out.print("Enter characters to be capitalized: ");
