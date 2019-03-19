07652a8745a0da245b9247bd4d05506a04e9b1e5
BADLZ <48454150+BADLZ@users.noreply.github.com>
2019-03-19 17:43:41 +0000

    Nothing special

--- Game-V2/src/main/Main.java
+++ Game-V2/src/main/Main.java
@@ -8,11 +8,7 @@ public class Main {
 
 
 	public static void main(String[] args) {
-		new SceneManager();
-		//SceneManager sm = new SceneManager();
-//		MainWindowManager game = new MainWindowManager();
-//		MainScreen window = new MainScreen();
-//		window.getFrame().setVisible(true);
+		new SceneManager();		
 	}
 	
 	
