71b03decf435ee502c3a3a4a016b8a4428cc302e
pedro <pncdias@hotmail.com>
2019-03-19 17:45:12 +0000

    New Acessories added (on going)

---
+++ Game-V2/src/acessorios/Chapeu.java
@@ -0,0 +1,24 @@
+package acessorios;
+
+import Personagens.Personagens;
+
+public class Chapeu extends Acessorios {
+
+	@Override
+	public void itemStats(Personagens p, String weapon, int lvlArma) {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void choseWeapon(Personagens p) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void evolutionCost(Personagens p, int lvlArma) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ Game-V2/src/acessorios/Tronco.java
@@ -0,0 +1,25 @@
+package acessorios;
+
+import Personagens.Personagens;
+
+public class Tronco extends Acessorios {
+
+	@Override
+	public void itemStats(Personagens p, String weapon, int lvlArma) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void choseWeapon(Personagens p) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void evolutionCost(Personagens p, int lvlArma) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
